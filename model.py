c2f81e3f5b5de3e5f45ec64366209470bf324d01
Huy Anh <huyanh995@gmail.com>
2021-05-19 01:13:35 +0700

    Finalize model

--- model.py
+++ model.py
@@ -16,39 +16,69 @@ import tensorflow.keras.backend as K
 
 
 class CustomAttention(keras.layers.Layer):
-    def __init__(self, pre_layer, input_dims):
+    def __init__(self, n_classes):
+        # Creating Attention Layer
         super(CustomAttention, self).__init__()
-
-    def call(self, inputs):
-        pass
+        self.dense = Dense(n_classes, activation='linear', use_bias=False)
+        self.permute = Permute((2,1))
+        self.activation = Activation('softmax')
+        self.attention = Lambda(lambda x: K.batch_dot(x[0], x[1]))
+        
+    def call(self, inputs, n_classes):
+        x = self.dense(inputs)
+        x = self.permute(a)
+        alpha = self.activation(x)
+        return self.attention([alpha, inputs])
 
 
 class HierarchicalModel(keras.Model):
     def __init__(
-        self,  
-        n_chapter_classes, 
-        n_heading_classes, 
-        n_sub_heading_classes, 
-        n_country_extension_classes,
-        num_words=5000,
-        embed_size = 512,
-        max_sequence_length=200, 
-        n_classes=100,
-        name = 'hierarchical_clf'):
+                self,  
+                n_chapter_classes, 
+                n_heading_classes, 
+                n_sub_heading_classes, 
+                n_country_extension_classes,
+                num_words=5000,
+                embed_size = 512,
+                max_sequence_length=200, 
+                n_classes=100,
+                name = 'hierarchical_clf'):
+                
         super(HierarchicalModel, self).__init__(name=name, **kwargs)
 
-        self.input = Input(shape=(max_sequence_length,))
-        self.embbed = Embedding(num_words, embed_size)
+        #self.input = Input(shape=(max_sequence_length,))
+        self.embedding = Embedding(num_words, embed_size)
         self.bilstm = Bidirectional(LSTM(max_sequence_length, return_sequences=True), merge_mode='concat')
-        #self.attention = CustomAttention(pre_layer, input_dims)
+        self.attention = CustomAttention(n_classes)
+        self.fc = Dense(1, activation='linear')
+        self.reshape = Reshape((n_classes,))
+
         self.fc_1 = Dense(n_classes, activation='relu')
         self.fc_2 = Dense(n_classes, activation='relu')
         self.fc_3 = Dense(n_classes, activation='relu')
         self.fc_4 = Dense(n_classes, activation='relu')
+
         self.chapter_output = Dense(n_chapter_classes, activation='sigmoid')
         self.heading_output = Dense(n_heading_classes, activation='relu')
         self.sub_heading_output = Dense(n_sub_heading_classes, activation='sigmoid')
         self.country_extension_output = Dense(n_country_extension_classes, activation='sigmoid')
 
-    def call(self):
-        pass
+    def call(self, inputs):
+        x = self.embedding(inputs)
+        x = self.bilstm(x)
+        x = self.attention(x)
+        x = self.reshape(self.fc(x))
+        fc1 = self.fc_1(x)
+        fc2 = self.fc_2(fc1)
+        fc3 = self.fc_3(fc2)
+        fc4 = self.fc_4(fc3)
+        chapter = self.chapter_output(fc1)
+        heading = self.heading_output(fc2)
+        sub_heading = self.sub_heading_output(fc3)
+        country_extension = self.country_extension_output(fc4)
+
+        return [chapter, heading, sub_heading, country_extension]
+
+        
+
+        
